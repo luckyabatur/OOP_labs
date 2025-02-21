f6bd3d27aab609fd865176066eed58b4ce5f6820
luckyabatur <dekhterenkodmitrii@gmail.com>
2025-02-21 19:51:48 +0300

    constructor correction 2

--- lab2/src/Eleven.cpp
+++ lab2/src/Eleven.cpp
@@ -4,10 +4,10 @@ Eleven::Eleven() : _size(0), _array{nullptr} {}
 
 Eleven::Eleven(const size_t &n, unsigned char t)
 {
-    if (!(t >= '0' && t <= '9' || t == 'A'))
+    if (!(t >= '0' && t <= '9' || t == 'A') || (n == 0) || (n == 1 && t == '0'))
     {
         delete[] _array;
-        throw std::invalid_argument("String constructor error");
+        throw std::invalid_argument("Char constructor error");
     }
     _array = new unsigned char[n];
     for (size_t i = 0; i < n; ++i)
